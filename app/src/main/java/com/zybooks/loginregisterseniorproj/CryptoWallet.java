aaad506e1962d7fe64a2df52bdfcb5ca64a3cee3
Jordan Clouden <clouden.jordan@gmail.com>
2022-03-25 20:36:18 -0400

    added button for transactions in crypto wallet

--- app/src/main/java/com/zybooks/loginregisterseniorproj/CryptoWallet.java
+++ app/src/main/java/com/zybooks/loginregisterseniorproj/CryptoWallet.java
@@ -4,18 +4,34 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 //Jordan
 public class CryptoWallet extends AppCompatActivity {
-
+private Button button6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crypto_wallet);
         getSupportActionBar().setTitle("Wallet Address");
+
+        button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCryptoTransactions();
+            }
+        });
     }
+public void openCryptoTransactions() {
+    Intent intent = new Intent(this, CryptoTransactions.class);
+    startActivity(intent);
+
+}
+
 
     public void showAlertDialog(View v) {
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
