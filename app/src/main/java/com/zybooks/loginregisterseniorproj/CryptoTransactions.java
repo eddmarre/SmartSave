21b4cae60e17c3ae044327dabece892ac3f216ae
Jordan Clouden <clouden.jordan@gmail.com>
2022-03-28 14:06:29 -0400

    added temporary button to bypass login and worked on transaction page

---
+++ app/src/main/java/com/zybooks/loginregisterseniorproj/CryptoTransactions.java
@@ -0,0 +1,23 @@
+package com.zybooks.loginregisterseniorproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class CryptoTransactions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crypto_transactions);
+
+        //Button button = (Button) findViewById(R.id.sendbtn1);
+
+
+    }
+
+}
