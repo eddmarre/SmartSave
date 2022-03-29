94aec4bd21ee91d9dfd5d1ae875040aa66dabc3c
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2022-03-28 22:44:08 -0400

    Fixed Issue with cryptoTransactionList

--- app/src/main/java/com/zybooks/loginregisterseniorproj/CryptoTransactions.java
+++ app/src/main/java/com/zybooks/loginregisterseniorproj/CryptoTransactions.java
@@ -52,11 +52,11 @@ public EditText _usdvalueid;
 
     public void doSend(View view) {
         Intent currentintent=new Intent(this,CryptoTransactionsList.class);
-       // Intent intent=new Intent(CryptoTransactions.this,CryptoTransactionsList.class);
-//        intent.putExtra("BTCVal",_btcvalueid.getText().toString());
-//        intent.putExtra("Recip",_recipientID.getText().toString());
-//        intent.putExtra("DescID",_descid.getText().toString());
-//        intent.putExtra("UsdID",_usdvalueid.getText().toString());
+        Intent intent=new Intent(CryptoTransactions.this,CryptoTransactionsList.class);
+        intent.putExtra("BTCVal",_btcvalueid.getText().toString());
+        intent.putExtra("Recip",_recipientID.getText().toString());
+        intent.putExtra("DescID",_descid.getText().toString());
+        intent.putExtra("UsdID",_usdvalueid.getText().toString());
         startActivity(currentintent);
     }
 
