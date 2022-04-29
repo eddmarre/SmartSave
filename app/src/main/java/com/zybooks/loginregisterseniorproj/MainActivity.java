f048db480f3f60b4dc8f48c251ec2b68d4d338b8
eddmarre <50213623+eddmarre@users.noreply.github.com>
2022-04-29 19:37:44 -0400

    Login with Family Member From home Page

--- app/src/main/java/com/zybooks/loginregisterseniorproj/MainActivity.java
+++ app/src/main/java/com/zybooks/loginregisterseniorproj/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+
 //Tino & Eddie
 public class MainActivity extends AppCompatActivity {
     //Tino
@@ -30,8 +31,8 @@ public class MainActivity extends AppCompatActivity {
     private Intent loginSuccessIntent;
 
 
-    private static final String SHARED_PREFS="sharedPrefs";
-    private static final String TEXT="text";
+    private static final String SHARED_PREFS = "sharedPrefs";
+    private static final String TEXT = "text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle("Login");
         //Tino
-        Intent recString= getIntent();
+        Intent recString = getIntent();
 
         entry1 = (EditText) findViewById(R.id.entry1); //make them findable by id
         entry4 = (EditText) findViewById(R.id.entry4);
@@ -48,148 +49,180 @@ public class MainActivity extends AppCompatActivity {
         register = (Button) findViewById(R.id.Register);
         login = (Button) findViewById(R.id.Login);
         //Eddie
-        userNames=new ArrayList<>();
-        passwords=new ArrayList<>();
-        
-        isValidLogin=false;
+        userNames = new ArrayList<>();
+        passwords = new ArrayList<>();
+
+        isValidLogin = false;
 
         CheckIfLoggedIn();
 
         CreateOrAddUserTable();
     }
+
     //Eddie
     private void CheckIfLoggedIn() {
-        if(!GetCurrentUserName().equals(""))
-        {
-            Intent n = new Intent(this,UserMainMenu.class);
+        if (!GetCurrentUserName().equals("")) {
+            Intent n = new Intent(this, UserMainMenu.class);
             startActivity(n);
         }
     }
 
     //Eddie
     private void CreateOrAddUserTable() {
-        _SQLTableManager =new SQLTableManager(this);
+        _SQLTableManager = new SQLTableManager(this);
     }
+
     /*On Start and On Stop will check to make sure User logins don't try to run after activity
     is shut down */
     //Eddie
     @Override
     protected void onStart() {
         super.onStart();
-        isActivityActive=true;
+        isActivityActive = true;
     }
+
     //Eddie
     @Override
     protected void onStop() {
         super.onStop();
-        isActivityActive=false;
+        isActivityActive = false;
     }
+
     //Eddie
     public void LoginToApp(View v) { //button to login, checks if username and password matches registration
         CheckIfValidUserNameAndPassword();
-        if(!isValidLogin && isActivityActive)
-        {
+        checkIfFamilyUserExist();
+        if (!isValidLogin && isActivityActive) {
             Toast.makeText(this, "error couldn't login", Toast.LENGTH_SHORT).show();
         }
     }
+
     //Eddie
     private void CheckIfValidUserNameAndPassword() {
         //retrieves the data from the database
-        Cursor accountUserTableData= _SQLTableManager.GetTableData("Account_User");
-        if(accountUserTableData.getCount()==0)
-        {
+        Cursor accountUserTableData = _SQLTableManager.GetTableData("Account_User");
+        if (accountUserTableData.getCount() == 0) {
             Toast.makeText(this, "error, database is empty", Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
+        } else {
             try {
                 //reads all data from the database and stores emails & passwords
-                while(accountUserTableData.moveToNext()) {
-                    int passwordIndex=6;
-                    int emailIndex =4;
+                while (accountUserTableData.moveToNext()) {
+                    int passwordIndex = 6;
+                    int emailIndex = 4;
                     userNames.add(accountUserTableData.getString(emailIndex));
                     passwords.add(accountUserTableData.getString(passwordIndex));
                 }
-            }
-            catch(Exception e)
-            {
+            } catch (Exception e) {
                 Toast.makeText(this, "error, couldn't show user data", Toast.LENGTH_SHORT).show();
             }
         }
         //nested foreach loop used to check if the same username and password from the same database row matches
         //**** email is being used as the userName ****
-        for (String _username: userNames)
-        {
-            for (String _password:passwords)
-            {
-                if (username.getText().toString().equals(_username) && (password.getText().toString().equals(_password)))
-                {
+        for (String _username : userNames) {
+            for (String _password : passwords) {
+                if (username.getText().toString().equals(_username) && (password.getText().toString().equals(_password))) {
                     Toast.makeText(MainActivity.this, "Successful login", Toast.LENGTH_SHORT).show();
-                    isValidLogin=true;
+                    isValidLogin = true;
                     SaveUserName(username.getText().toString());
                     ChangeActivityAfterLogin(_username);
-                }
-                else
-                {
-                 isValidLogin=false;   
+                } else {
+                    isValidLogin = false;
                 }
             }
         }
     }
+
     //Eddie
     private void ChangeActivityAfterLogin(String userName) {
-       // InsertAllTablesGarbageValues();
-        loginSuccessIntent= new Intent(this, UserMainMenu.class);
+        // InsertAllTablesGarbageValues();
+        loginSuccessIntent = new Intent(this, UserMainMenu.class);
         startActivity(loginSuccessIntent);
         finish();
     }
+
     //Eddie Debug Function
     //**Uncomment Line 133 to work**
-    private void InsertAllTablesGarbageValues()
-    {
+    private void InsertAllTablesGarbageValues() {
         SQLTableManager SQLTableManager = new SQLTableManager(this);
-        boolean accountUserSuccess=SQLTableManager.InsertAccountUser("Admin","User","xx/xx/xxxx","admin@savesmart.com",1234567890,"adminPassword");
+        boolean accountUserSuccess = SQLTableManager.InsertAccountUser("Admin", "User", "xx/xx/xxxx", "admin@savesmart.com", 1234567890, "adminPassword");
 
-        boolean cryptoWalletSuccess=SQLTableManager.InsertCryptoWallet("Admin","AAA",1,100,"today");
-        boolean stockWalletSuccess=SQLTableManager.InsertStockWallet("Admin","BTC",1,100,"today");
+        boolean cryptoWalletSuccess = SQLTableManager.InsertCryptoWallet("Admin", "AAA", 1, 100, "today");
+        boolean stockWalletSuccess = SQLTableManager.InsertStockWallet("Admin", "BTC", 1, 100, "today");
 
-        boolean userIncomeSuccess=SQLTableManager.InsertUserIncome("Admin","nothing yet",500,"today");
-        boolean userExpenseSuccess =SQLTableManager.InsertUserExpense("Admin","nothing yet",500,"today");
+        boolean userIncomeSuccess = SQLTableManager.InsertUserIncome("Admin", "nothing yet", 500, "today");
+        boolean userExpenseSuccess = SQLTableManager.InsertUserExpense("Admin", "nothing yet", 500, "today");
 
-        boolean familyUserSuccess=SQLTableManager.InsertFamilyUser("Admin","COOLGUY","John","Doe","today","Son");
+        boolean familyUserSuccess = SQLTableManager.InsertFamilyUser("Admin", "COOLGUY", "John", "Doe", "today", "Son");
 
-        boolean familyUserIncomeSuccess=SQLTableManager.InsertFamilyUserIncome("COOLGUY","nothing yet",500,"today");
-        boolean familyUserExpenseSuccess=SQLTableManager.InsertFamilyUserExpense("COOLGUY","nothing yet",500,"today");
+        boolean familyUserIncomeSuccess = SQLTableManager.InsertFamilyUserIncome("COOLGUY", "nothing yet", 500, "today");
+        boolean familyUserExpenseSuccess = SQLTableManager.InsertFamilyUserExpense("COOLGUY", "nothing yet", 500, "today");
 
-        if(cryptoWalletSuccess&&stockWalletSuccess&&userIncomeSuccess&& userExpenseSuccess &&familyUserSuccess&&familyUserIncomeSuccess&&familyUserExpenseSuccess)
-        {
+        if (cryptoWalletSuccess && stockWalletSuccess && userIncomeSuccess && userExpenseSuccess && familyUserSuccess && familyUserIncomeSuccess && familyUserExpenseSuccess) {
             //if they all insert successfully do nothing
-        }
-        else
-        {
-            Toast.makeText(this,"one of the table inserts were messed up",Toast.LENGTH_SHORT);
+        } else {
+            Toast.makeText(this, "one of the table inserts were messed up", Toast.LENGTH_SHORT);
         }
     }
 
     private void SaveUserName(String userName) {
-       // SharedPreferences sharedPreferences =getSharedPreferences("sharedPreferencs",MODE_PRIVATE);
-        SharedPreferences sharedPreferences=getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
-        SharedPreferences.Editor editor=sharedPreferences.edit();
-        editor.putString(TEXT,userName);
+        // SharedPreferences sharedPreferences =getSharedPreferences("sharedPreferencs",MODE_PRIVATE);
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(TEXT, userName);
         editor.apply();
     }
-    
-     public String GetCurrentUserName()
-    {
-        SharedPreferences sharedPreferences=getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
-        String currentUserName=sharedPreferences.getString(TEXT,"");
+
+    public String GetCurrentUserName() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        String currentUserName = sharedPreferences.getString(TEXT, "");
         return currentUserName;
     }
+
     //Tino
     public void Register(View v) { //switch to registration activity
         Intent n = new Intent(this, Register.class);
         startActivity(n);
     }
+
+    public void checkIfFamilyUserExist() {
+
+        // 1) Grab Saved Data
+        SQLTableManager anotherTable = new SQLTableManager(this);
+        //create temp data holder
+        Cursor someOtherTable = anotherTable.GetTableData("Account_User_Family_User");
+        //create a list to store all the data from the table
+
+        ArrayList<FamilyUser> users = new ArrayList<>();
+        //make sure table is not empty
+        if (someOtherTable.getCount() == 0) {
+            Toast.makeText(this, "error, database is empty", Toast.LENGTH_SHORT).show();
+        } else {
+            //if not empty try
+            try {
+                //reads all data from the database and compares to what we are looking for
+                while (someOtherTable.moveToNext()) {
+                    users.add(new FamilyUser(someOtherTable.getString(1), someOtherTable.getString(2), someOtherTable.getString(3), someOtherTable.getString(4),
+                            someOtherTable.getString(5)));
+                }
+                //toss error if nothing was found in the data search
+            } catch (Exception e) {
+                Toast.makeText(this, "error, couldn't show user data", Toast.LENGTH_SHORT).show();
+            }
+
+
+            for (FamilyUser user : users) {
+                if (username.getText().toString().equals(user.getUserId()) && (password.getText().toString().equals(user.getRelationToOwner()))) {
+                    Toast.makeText(MainActivity.this, "Successful login", Toast.LENGTH_SHORT).show();
+                    isValidLogin = true;
+                    Intent n = new Intent(this, FamilyMemberMainMenu.class);
+                    startActivity(n);
+                } else {
+                    isValidLogin = false;
+                }
+            }
+        }
+    }
+
 
     //Eddie
     //for debugging purposes
